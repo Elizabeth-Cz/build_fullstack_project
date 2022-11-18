ddfae167a132b97ea6905ddb0b27f8baa6dea2b8
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-18 17:33:39 +0100

    feat[App]: links to register and login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import { useAuth } from "./context/authContext";
 
 function App() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  if (user) return <div>{user.email}</div>;
-  return <div className="App">Hello world</div>;
+  if (user)
+    return (
+      <div>
+        <h2>Helloo user {user.email}</h2>
+        <button onClick={logout}>logout</button>
+        <Link to="/profile" />
+      </div>
+    );
+  return (
+    <div className="App">
+      <h1>App name</h1>
+      <h2>Welcome!</h2>
+      <button onClick={() => navigate("/login")}>Login</button>
+      <button onClick={() => navigate("/register")}>Sign up</button>
+    </div>
+  );
 }
 
 export default App;
